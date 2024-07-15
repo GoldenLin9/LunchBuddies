bff330111c5a7abb1bf706e74bb338ccb877cdfa
Golden Lin <goldenlin0909@gmail.com>
2024-07-15 13:32:58 -0400

    Join/Leave API, DEL/GET Booking

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -11,6 +11,8 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("", views.test, name="test"),
     path("book/", views.Book.as_view(), name="book"),
+    path("join/", views.Join.as_view(), name="join"),
+    path("leave/", views.Leave.as_view(), name="leave"),
 
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
