570c0e4943f0e6e7294b8cb15c452732f9007b90
Evan Naderi <ev230759@ucf.edu>
2024-07-15 12:56:13 -0400

    fixed color on headers

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,35 @@
 // src/components/Header.js
 import React from 'react';
 import { Box, Flex, Heading, Spacer, Button } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const Header = () => (
-  <Box bg="teal.500" py={4}>
-    <Flex maxW="container.xl" mx="auto" align="center" color="white">
-      <Heading size="lg">Lunch Meetup</Heading>
-      <Spacer />
-      <Button as={Link} to="/" variant="ghost" mr={2}>Home</Button>
-      <Button as={Link} to="/bookings" variant="ghost" mr={2}>Bookings</Button>
-      <Button as={Link} to="/profile" variant="ghost">Profile</Button>
-    </Flex>
-  </Box>
-);
+const Header = () => {
+  const location = useLocation();
+
+  const NavButton = ({ to, children }) => (
+    <Button
+      as={Link}
+      to={to}
+      variant={location.pathname === to ? "solid" : "ghost"}
+      bg={location.pathname === to ? "teal.600" : "transparent"}
+      _hover={{ bg: "teal.600" }}
+      mr={2}
+    >
+      {children}
+    </Button>
+  );
+
+  return (
+    <Box bg="teal.500" py={4}>
+      <Flex maxW="container.xl" mx="auto" align="center" color="white">
+        <Heading size="lg">Lunch Meetup</Heading>
+        <Spacer />
+        <NavButton to="/">Home</NavButton>
+        <NavButton to="/bookings">Bookings</NavButton>
+        <NavButton to="/profile">Profile</NavButton>
+      </Flex>
+    </Box>
+  );
+};
 
 export default Header;
-
