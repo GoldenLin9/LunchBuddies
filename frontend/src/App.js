bab58c2362897ad1540d9381c1072660c92dd465
Evan Naderi <ev230759@ucf.edu>
2024-07-17 10:47:03 -0400

    login routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { ChakraProvider, Box } from '@chakra-ui/react';
+import PrivateRoute from './components/PrivateRoute';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -27,15 +28,17 @@ function App() {
           <Box minHeight="100vh" display="flex" flexDirection="column">
             <Header />
             <Box flex={1}>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/bookings" element={<Bookings />} />
-                <Route path="/chat/:id" element={<Chat />} />
-              </Routes>
-            </Box>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route element={<PrivateRoute />}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/bookings" element={<Bookings />} />
+                        <Route path="/chat/:id" element={<Chat />} />
+                    </Route>
+                    <Route path="*" element={<Navigate to="/login" replace />} />
+                </Routes></Box>
             <Footer />
           </Box>
           
