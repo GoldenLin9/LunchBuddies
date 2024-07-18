1951d0a842c1c1cb3498c7eaef76d9e54b378431
Evan Naderi <evanvnaderi@gmail.com>
2024-07-17 20:36:10 -0400

    login link to register

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,20 +1,14 @@
-// src/pages/Login.js
 import React, { useState, useContext } from 'react';
-import { Box, VStack, Heading, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
-
+import { Box, VStack, Heading, FormControl, FormLabel, Input, Button, HStack, Text } from '@chakra-ui/react';
+import { useNavigate, Link } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 
-
-
 export default function Login() {
-
   let { login } = useContext(AuthContext);
-
   
-
   let [username, setUsername] = useState('');
   let [password, setPassword] = useState('');
-
+  const navigate = useNavigate();
 
   let updateLogin = (e) => {
     setUsername(e.target.value);
@@ -23,29 +17,35 @@ export default function Login() {
   let updatePassword = (e) => {
     setPassword(e.target.value);
   }
-
   
-  function handleLogin() {
+  function handleLogin(e) {
+    e.preventDefault(); // Prevent default form submission
     login(username, password);
+    navigate('/');
   }
 
-
-  return <>
-
+  return (
     <Box maxW="container.sm" mx="auto" py={10}>
-      <VStack spacing={6}>
-        <Heading>Login</Heading>
-        <FormControl>
-          <FormLabel>username</FormLabel>
-          <Input type="username" onChange = {updateLogin}/>
-        </FormControl>
-        <FormControl>
-          <FormLabel>Password</FormLabel>
-          <Input type="password" onChange = {updatePassword}/>
-        </FormControl>
-        <Button colorScheme="teal" width="full" onClick = {handleLogin}>Login</Button>
-      </VStack>
+      <form onSubmit={handleLogin}>
+        <VStack spacing={6}>
+          <Heading>Login</Heading>
+          <FormControl isRequired>
+            <FormLabel>Username</FormLabel>
+            <Input type="text" value={username} onChange={updateLogin} />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Password</FormLabel>
+            <Input type="password" value={password} onChange={updatePassword} />
+          </FormControl>
+          <Button type="submit" colorScheme="teal" width="full">Login</Button>
+          <HStack>
+            <Text>Don't have an account?</Text>
+            <Button as={Link} to="/register" variant="link" colorScheme="teal">
+              Register
+            </Button>
+          </HStack>
+        </VStack>
+      </form>
     </Box>
-  
-  </>
-};
+  );
+}
