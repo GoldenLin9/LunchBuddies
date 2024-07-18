40589dbd0aa40b6e3bb6455a1624a187f6ca8f88
GoldenLin9 <goldenlin0909@gmail.com>
2024-07-18 13:25:49 -0400

    updated title

--- backend/base/models.py
+++ backend/base/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
 class Booking(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, null=True, blank=True)
     
     meeting_time = models.DateTimeField()
     members = models.ManyToManyField(User, related_name='members')
